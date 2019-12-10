d36a87e9db8be925c8dc0452da8dfcae0582991c
Matt Wayne <matt@thinkdolabs.com>
2019-12-10 17:41:03 -0500

    Adding to database (to be continued).

--- redfetch.py
+++ redfetch.py
@@ -12,29 +12,36 @@ reddit = praw.Reddit(client_id='tiHEaLH1D4ds-w', client_secret='iMvbqDQxbW9FJIc5
 #     print(subreddit)
 # Cannot use attribute subreddit on a list
 
-learn_python_submissions = reddit.subreddit('learnpython')
-funny_submissions = reddit.subreddit('funny')
-askreddit_submissions = reddit.subreddit('AskReddit')
-todayilearned_submissions = reddit.subreddit('todayilearned')
-worldnews_submissions = reddit.subreddit('world_news')
+learn_python = reddit.subreddit('learnpython')
+funny = reddit.subreddit('funny')
+askreddit = reddit.subreddit('AskReddit')
+todayilearned = reddit.subreddit('todayilearned')
+worldnews = reddit.subreddit('world_news')
 
-submissions_list = [ learn_python_submissions, funny_submissions, askreddit_submissions, todayilearned_submissions, worldnews_submissions]
+subreddit_list = [learn_python, funny, askreddit, todayilearned, worldnews]
 
-# subreddit = reddit.subreddit('learnpython')
+def get_submissions():
+    submissions_list_of_lists = []
+    for subreddit in subreddit_list:
+        sub_title = submission.title
+        sub_url = submission.url
+        sub_id = submission.id
+        sub_score = submission.score
+        sub_comms_num = submission.num_comments
+    
+
+top_python_learn = subreddit.top()
 
 
-# top_python_learn = subreddit.top()
+db = dataset.connect('sqlite:///reddit_learnpython.db')
+table = db['reddit_submissions']
 
 
-# db = dataset.connect('sqlite:///reddit_learnpython.db')
-# table = db['reddit_submissions']
-
-
-# if __name__ == '__main__':
-#     for submission in top_python_learn:
-#         sub_title = submission.title
-#         sub_url = submission.url
-#         sub_id = submission.id
-#         sub_score = submission.score
-#         sub_comms_num = submission.num_comments
-#         table.insert(dict(title=sub_title, url=sub_url, sub_id=sub_id, score=sub_score, comms_num=sub_comms_num))
+if __name__ == '__main__':
+    for submission in top_python_learn:
+        sub_title = submission.title
+        sub_url = submission.url
+        sub_id = submission.id
+        sub_score = submission.score
+        sub_comms_num = submission.num_comments
+        table.insert(dict(title=sub_title, url=sub_url, sub_id=sub_id, score=sub_score, comms_num=sub_comms_num))
