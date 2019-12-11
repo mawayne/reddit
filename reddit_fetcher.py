588650483a364069a29873c7b68bc410a4b35994
Matt Wayne <matt@thinkdolabs.com>
2019-12-10 21:23:12 -0500

    Adding to database (to be continued).

--- reddit_fetcher.py
+++ reddit_fetcher.py
@@ -1,52 +1,46 @@
 import praw 
 import dataset
+from stuf import stuf
 
 reddit = praw.Reddit(client_id='tiHEaLH1D4ds-w', client_secret='iMvbqDQxbW9FJIc5ZtH2bVuwAcY', user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36')
-subreddit = reddit.subreddit('learnpython')
-submissions = subreddit.top()
 
-# top_learn_python_dict = { "title":[], \
-#                             "score":[], \
-#                             "id":[], \
-#                             "url":[], \
-#                             "comms_num": []}
+# sub_names = ['learnpython', 'funny', 'AskReddit', 'todayilearned', 'worldnews']
+# sub_names = ['learnpython', 'funny', 'askreddit', 'todayilearned', 'worldnews']
 
-# for submission in submissions:
-#     top_learn_python_dict["title"].append(submission.title)
-#     top_learn_python_dict["score"].append(submission.score)
-#     top_learn_python_dict["id"].append(submission.id)
-#     top_learn_python_dict["url"].append(submission.url)
-#     top_learn_python_dict["comms_num"].append(submission.num_comments)
+# for sub in sub_names:
+#     subreddit = reddit.subreddit(sub_names)
+#     print(subreddit)
+# Cannot use attribute subreddit on a list
 
-for submission in submissions:
-    title = submission.title
-    score = submission.score
-    sub_id= submission.id
-    url = submission.url
-    comms_num = submission.num_comments
+learn_python = reddit.subreddit('learnpython')
+funny = reddit.subreddit('funny')
+askreddit = reddit.subreddit('AskReddit')
+todayilearned = reddit.subreddit('todayilearned')
+worldnews = reddit.subreddit('world_news')
 
-# top_learn_python_dict = { "title": title, \
-#                             "score": score, \
-#                             "id": sub_id, \
-#                             "url": url, \
-#                             "comms_num": comms_num}
+subreddits = [learn_python, funny, askreddit, todayilearned, worldnews]
+
+# def get_submissions():
+for subreddit in subreddits:
+    new_submissions = subreddit.new() 
+    top_submissions = subreddit.top()
+    # hot_submissions = subreddit.hot()
+    # rising_submissions = subreddit.rising()
+    # gilded_submissions = subreddit.gilded()
+    # controversial_submissions = subreddit.controversial()
+print(new_submissions)
+print(top_submissions)
 
 
-# for submission in submissions:
-#     title = submission.title
-#     score = submission.score
-#     sub_id= submission.id
-#     url = submission.url
-#     comms_num = submission.num_comments
-#     top_learn_python_dict = { "title": title, \
-#                             "score": score, \
-#                             "id": sub_id, \
-#                             "url": url, \
-#                             "comms_num": comms_num}
+# db = dataset.connect('sqlite:///reddit_learnpython.db')
+# table = db['reddit_submissions']
+
 
 # if __name__ == '__main__':
-    # print(top_learn_python_dict)
-    # print(type(top_learn_python_dict))
-    # db = dataset.connect('sqlite:///reddit_learnpython.db')
-    # table = db['top posts']
-    # table.insert(top_learn_python_dict)
+#     for submission in top_python_learn:
+#         sub_title = submission.title
+#         sub_url = submission.url
+#         sub_id = submission.id
+#         sub_score = submission.score
+#         sub_comms_num = submission.num_comments
+#         table.insert(dict(title=sub_title, url=sub_url, sub_id=sub_id, score=sub_score, comms_num=sub_comms_num))
