16b97c0c245fc42c055cd169205bcd2aa151d469
Matt Wayne <matt@thinkdolabs.com>
2019-12-11 02:35:45 -0500

    Added top_submisions of r/learnpython.

--- reddit.py
+++ reddit.py
@@ -1,5 +1,4 @@
 import time
-
 import praw 
 import dataset
 
@@ -8,61 +7,52 @@ reddit = praw.Reddit(
     client_secret='iMvbqDQxbW9FJIc5ZtH2bVuwAcY',
     user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36')
 
-
 def get_submissions(subreddit_name):
     subreddit = reddit.subreddit(subreddit_name)
     top_submissions = subreddit.top()
     new_submissions = subreddit.new()
     hot_submissions = subreddit.hot()
-    # submissions = [top_submissions, new_submissions, hot_submissions]
+    rising_submissions = subreddit.rising()
+    gilded_submissions = subreddit.gilded()
+    controversial_submissions = subreddit.controversial()
     all_submissions = []
-    # all_submissions.extend(top_submissioins)
     for submission in top_submissions:
         submission_as_dict = {}
-        submission_as_dict['title'] = submission.title
-        # url
-        # so on
-        submission['category'] = 
-        all_submissions.append(submission)
-
-    for submission in new_submissions:
-        submission_as_dict = {}
-        submission_as_dict['title'] = submission.title
-        # url
-        # so on
-        submission['category'] = 'new'
-        all_submissions.append(submission)
+        submission_as_dict['subreddit'] = subreddit_name
+        submission_as_dict['category'] = 'top'
+        submission_as_dict['sub_title'] = submission.title
+        submission_as_dict['sub_url'] = submission.url
+        submission_as_dict['sub_id'] = submission.id 
+        submission_as_dict['sub_score'] = submission.score
+        submission_as_dict['sub_comms_num'] = submission.num_comments
+        all_submissions.append(submission_as_dict)
+    # for submission in new_submissions:
+    #     submission_as_dict = {}
+    #     submission_as_dict['title'] = submission.title
+    #     submission['category'] = 'new'
+    #     all_submissions.append(submission)
     return all_submissions
-
-
-        
-
-
-    
-    return top_submissions
 
 def test_get_submissions():
     subreddit_name = 'learnpython'
     top_submissions = get_submissions(subreddit_name)
     for submission in top_submissions:
-        assert submission.url
         assert submission.title
+        assert submission.url
+        assert submission.id
+        assert submission.score
+        assert submission.num_comments
+        
+# subreddits = ['learnpython', 'funny', 'AskReddit', 'todayilearned', 'worldnews']
+subreddits = ['learnpython']
 
+db = dataset.connect('sqlite:///reddit_learnpython.db')
+table = db['test_3']
 
 if __name__ == '__main__':
-    subreddit_name = 'learnpython'
-    top_submissions = get_submissions(subreddit_name)
-    # print(top_su)
-    # for submission in top_submissions:
-        # print(type(submission))
-        # print(dir(submission))
-        # print(dict(submission))
-        # print(submission._get_json_dict())
-        # print(submission.json_dict)
-        # time.sleep(1)
-        # print(submission.title)
     for subreddit in subreddits:
         submissions = get_submissions(subreddit)
         for submission in submissions:
-            submission_as_dict = create_dict_from_submission(submission)
-            table.insert(submission_as_dict)
+            create_dict_from_submission = dict(title=submission_as_dict['sub_title'], url=submission_as_dict['sub_url'], sub_id=submission_as_dict['sub_id'], score=submission_as_dict['sub_score'], comms_num=submission_as_dict['sub_comms_num'])
+            # submission_as_dict = create_dict_from_submission(submission)
+            table.insert(submission)
