37eafa252ff11c1bf6a07ade4e6d6483cdc54d48
Matt Wayne <matt@thinkdolabs.com>
2019-12-12 15:51:20 -0500

    Added more tests.

--- test_insert_sqlite.py
+++ test_insert_sqlite.py
@@ -16,7 +16,13 @@ def test_row_datatype():
     for row in table:
         assert str(type(row)) == "<class 'stuf.core.stuf'>"
 
-def test_subreddit_column():
+def test_subreddit_column_general():
+    result = db.query("SELECT subreddit FROM reddit_submissions GROUP BY subreddit")
+    for row in result:
+        subreddits = ['learnpython', 'funny', 'AskReddit', 'todayilearned', 'worldnews']
+        assert row['subreddit'] in subreddits
+
+def test_subreddit_column_specific():
         result = db.query("SELECT subreddit FROM reddit_submissions WHERE subreddit='learnpython'")
         for row in result:
                 assert row['subreddit'] == 'learnpython'
@@ -26,14 +32,41 @@ def test_url():
         for row in result:
                 assert 'https://' in row['sub_url']
 
+def test_categories():
+        result = db.query("SELECT category FROM reddit_submissions GROUP BY category")
+        for row in result:
+                if row['category'] == 'controversial':
+                        assert row['category'] == 'controversial'
+                elif row['category'] == 'hot':
+                        assert row['category'] == 'hot'
+                elif row['category'] == 'top':
+                        assert row['category'] == 'top'
+                elif row['category'] == 'new':
+                        assert row['category'] == 'new'
+                elif row['category'] == 'rising':
+                        assert row['category'] == 'rising'
 
- 
-# def test_upload_reddit_users():
-#     test_table_columns = 
+def test_categories_2():
+        result = db.query("SELECT category FROM reddit_submissions GROUP BY category")
+        categories = ['top', 'new', 'hot', 'rising', 'controversial']
+        for row in result:
+            assert row['category'] in categories
+
+def test_datatypes():
+        result = db.query("SELECT * from reddit_submissions LIMIT 1")
+        for row in result:
+                assert type(row['subreddit']) == str
+                assert type(row['category']) == str
+                assert type(row['sub_title']) == str
+                assert type(row['sub_url']) == str
+                assert type(row['sub_comms_num']) == int
+                assert type(row['is_original_content']) == bool
+                assert type(row['upvotes_percentage']) == float
+                assert type(row['time_created_utc']) == float
 
 
-# def test_
-# result = db.query('SELECT sub_comms_num, COUNT(*) scm FROM test ORDER BY scm DESC')
-# print(result)
+
+
+
 
 
