8e891025342c59e4eb20c897628107020983003f
Matt Wayne <matt@thinkdolabs.com>
2019-12-11 22:36:35 -0500

    Removed unecessary code.

--- reddit.py
+++ reddit.py
@@ -123,77 +123,6 @@ def get_submissions(subreddit_name):
         all_submissions.append(submission_as_dict)       
     return all_submissions
 
-# def add_submissions(subreddit_name):
-#     subreddit = reddit.subreddit(subreddit_name)
-#     top_submissions = subreddit.top()
-#     new_submissions = subreddit.new()
-#     hot_submissions = subreddit.hot()
-#     rising_submissions = subreddit.rising()
-#     controversial_submissions = subreddit.controversial()
-#     all_added_submissions = []
-#     time.sleep = 1
-#     for submission in top_submissions:
-#         submission_as_dict = {}
-
-#         all_added_submissions.append(submission_as_dict)       
-#     for submission in new_submissions:
-#         submission_as_dict = {}
-#         submission_as_dict['sub_author'] = submission.author
-#         submission_as_dict['time_created_utc'] = submission.created_utc 
-#         submission_as_dict['is_original_content'] = submission.is_original_content
-#         submission_as_dict['self_post'] = submission.is_self
-#         submission_as_dict['sub_full_name'] = submission.name
-#         submission_as_dict['nsfw'] = submission.over_18
-#         submission_as_dict['permalink'] = submission.permalink
-#         submission_as_dict['sub_selftext'] = submission.selftext
-#         submission_as_dict['sub_stickied'] = submission.stickied
-#         submission_as_dict['sub_subreddit'] = submission.subreddit
-#         submission_as_dict['upvotes_percentage'] = submission.upvote_ratio
-#         all_added_submissions.append(submission_as_dict)
-#     for submission in hot_submissions:
-#         submission_as_dict = {}
-#         submission_as_dict['sub_author'] = submission.author
-#         submission_as_dict['time_created_utc'] = submission.created_utc 
-#         submission_as_dict['is_original_content'] = submission.is_original_content
-#         submission_as_dict['self_post'] = submission.is_self
-#         submission_as_dict['sub_full_name'] = submission.name
-#         submission_as_dict['nsfw'] = submission.over_18
-#         submission_as_dict['permalink'] = submission.permalink
-#         submission_as_dict['sub_selftext'] = submission.selftext
-#         submission_as_dict['sub_stickied'] = submission.stickied
-#         submission_as_dict['sub_subreddit'] = submission.subreddit
-#         submission_as_dict['upvotes_percentage'] = submission.upvote_ratio
-#         all_added_submissions.append(submission_as_dict)
-#     for submission in rising_submissions:
-#         submission_as_dict = {}
-#         submission_as_dict['sub_author'] = submission.author
-#         submission_as_dict['time_created_utc'] = submission.created_utc 
-#         submission_as_dict['is_original_content'] = submission.is_original_content
-#         submission_as_dict['self_post'] = submission.is_self
-#         submission_as_dict['sub_full_name'] = submission.name
-#         submission_as_dict['nsfw'] = submission.over_18
-#         submission_as_dict['permalink'] = submission.permalink
-#         submission_as_dict['sub_selftext'] = submission.selftext
-#         submission_as_dict['sub_stickied'] = submission.stickied
-#         submission_as_dict['sub_subreddit'] = submission.subreddit
-#         submission_as_dict['upvotes_percentage'] = submission.upvote_ratio
-#         all_added_submissions.append(submission_as_dict)
-#     for submission in controversial_submissions:
-#         submission_as_dict = {}
-#         submission_as_dict['sub_author'] = submission.author
-#         submission_as_dict['time_created_utc'] = submission.created_utc 
-#         submission_as_dict['is_original_content'] = submission.is_original_content
-#         submission_as_dict['self_post'] = submission.is_self
-#         submission_as_dict['sub_full_name'] = submission.name
-#         submission_as_dict['nsfw'] = submission.over_18
-#         submission_as_dict['permalink'] = submission.permalink
-#         submission_as_dict['sub_selftext'] = submission.selftext
-#         submission_as_dict['sub_stickied'] = submission.stickied
-#         submission_as_dict['sub_subreddit'] = submission.subreddit
-#         submission_as_dict['upvotes_percentage'] = submission.upvote_ratio
-#         all_added_submissions.append(submission_as_dict)
-#     return all_added_submissions
-
 subreddits = ['learnpython', 'funny', 'AskReddit', 'todayilearned', 'worldnews']
 
 db = dataset.connect('sqlite:///sqlite_testing.db')
@@ -204,8 +133,4 @@ if __name__ == '__main__':
         submissions = get_submissions(subreddit)
         for submission in submissions:
             table.insert(submission)
-    # for subreddit in subreddits:
-    #     submissions = add_submissions(subreddit)
-    #     for submission in submissions:
-    #         table.insert(submission)
     
