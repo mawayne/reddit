e370a876b48d190e3fe7ef25f117642596558ebf
Matt Wayne <matt@thinkdolabs.com>
2019-12-11 20:36:03 -0500

    Could not update database with added code (sqlalchemy.exc.InterfaceError: (sqlite3.InterfaceError) Error binding parameter 0 - probably unsupported type.)

--- reddit.py
+++ reddit.py
@@ -13,7 +13,6 @@ def get_submissions(subreddit_name):
     new_submissions = subreddit.new()
     hot_submissions = subreddit.hot()
     rising_submissions = subreddit.rising()
-    gilded_submissions = subreddit.gilded()
     controversial_submissions = subreddit.controversial()
     all_submissions = []
     time.sleep = 1
@@ -69,25 +68,99 @@ def get_submissions(subreddit_name):
         all_submissions.append(submission_as_dict)       
     return all_submissions
 
-def test_get_submissions():
-    subreddit_name = 'learnpython'
-    top_submissions = get_submissions(subreddit_name)
+def add_submissions(subreddit_name):
+    subreddit = reddit.subreddit(subreddit_name)
+    top_submissions = subreddit.top()
+    new_submissions = subreddit.new()
+    hot_submissions = subreddit.hot()
+    rising_submissions = subreddit.rising()
+    controversial_submissions = subreddit.controversial()
+    all_added_submissions = []
+    time.sleep = 1
     for submission in top_submissions:
-        assert submission.title
-        assert submission.url
-        assert submission.id
-        assert submission.score
-        assert submission.num_comments
-        
+        submission_as_dict = {}
+        submission_as_dict['sub_author'] = submission.author
+        submission_as_dict['time_created_utc'] = submission.created_utc 
+        submission_as_dict['is_original_content'] = submission.is_original_content
+        submission_as_dict['self_post'] = submission.is_self
+        submission_as_dict['sub_full_name'] = submission.name
+        submission_as_dict['nsfw'] = submission.over_18
+        submission_as_dict['permalink'] = submission.permalink
+        submission_as_dict['sub_selftext'] = submission.selftext
+        submission_as_dict['sub_stickied'] = submission.stickied
+        submission_as_dict['sub_subreddit'] = submission.subreddit
+        submission_as_dict['upvotes_percentage'] = submission.upvote_ratio
+        all_added_submissions.append(submission_as_dict)       
+    for submission in new_submissions:
+        submission_as_dict = {}
+        submission_as_dict['sub_author'] = submission.author
+        submission_as_dict['time_created_utc'] = submission.created_utc 
+        submission_as_dict['is_original_content'] = submission.is_original_content
+        submission_as_dict['self_post'] = submission.is_self
+        submission_as_dict['sub_full_name'] = submission.name
+        submission_as_dict['nsfw'] = submission.over_18
+        submission_as_dict['permalink'] = submission.permalink
+        submission_as_dict['sub_selftext'] = submission.selftext
+        submission_as_dict['sub_stickied'] = submission.stickied
+        submission_as_dict['sub_subreddit'] = submission.subreddit
+        submission_as_dict['upvotes_percentage'] = submission.upvote_ratio
+        all_added_submissions.append(submission_as_dict)
+    for submission in hot_submissions:
+        submission_as_dict = {}
+        submission_as_dict['sub_author'] = submission.author
+        submission_as_dict['time_created_utc'] = submission.created_utc 
+        submission_as_dict['is_original_content'] = submission.is_original_content
+        submission_as_dict['self_post'] = submission.is_self
+        submission_as_dict['sub_full_name'] = submission.name
+        submission_as_dict['nsfw'] = submission.over_18
+        submission_as_dict['permalink'] = submission.permalink
+        submission_as_dict['sub_selftext'] = submission.selftext
+        submission_as_dict['sub_stickied'] = submission.stickied
+        submission_as_dict['sub_subreddit'] = submission.subreddit
+        submission_as_dict['upvotes_percentage'] = submission.upvote_ratio
+        all_added_submissions.append(submission_as_dict)
+    for submission in rising_submissions:
+        submission_as_dict = {}
+        submission_as_dict['sub_author'] = submission.author
+        submission_as_dict['time_created_utc'] = submission.created_utc 
+        submission_as_dict['is_original_content'] = submission.is_original_content
+        submission_as_dict['self_post'] = submission.is_self
+        submission_as_dict['sub_full_name'] = submission.name
+        submission_as_dict['nsfw'] = submission.over_18
+        submission_as_dict['permalink'] = submission.permalink
+        submission_as_dict['sub_selftext'] = submission.selftext
+        submission_as_dict['sub_stickied'] = submission.stickied
+        submission_as_dict['sub_subreddit'] = submission.subreddit
+        submission_as_dict['upvotes_percentage'] = submission.upvote_ratio
+        all_added_submissions.append(submission_as_dict)
+    for submission in controversial_submissions:
+        submission_as_dict = {}
+        submission_as_dict['sub_author'] = submission.author
+        submission_as_dict['time_created_utc'] = submission.created_utc 
+        submission_as_dict['is_original_content'] = submission.is_original_content
+        submission_as_dict['self_post'] = submission.is_self
+        submission_as_dict['sub_full_name'] = submission.name
+        submission_as_dict['nsfw'] = submission.over_18
+        submission_as_dict['permalink'] = submission.permalink
+        submission_as_dict['sub_selftext'] = submission.selftext
+        submission_as_dict['sub_stickied'] = submission.stickied
+        submission_as_dict['sub_subreddit'] = submission.subreddit
+        submission_as_dict['upvotes_percentage'] = submission.upvote_ratio
+        all_added_submissions.append(submission_as_dict)
+    return all_added_submissions
+
 subreddits = ['learnpython', 'funny', 'AskReddit', 'todayilearned', 'worldnews']
 
-db = dataset.connect('sqlite:///reddit_learnpython.db')
-table = db['test_13']
-
-
+db = dataset.connect('sqlite:///test_data.db')
+table = db['reddit_submissions']
 
 if __name__ == '__main__':
+    # for subreddit in subreddits:
+    #     submissions = get_submissions(subreddit)
+    #     for submission in submissions:
+    #         table.insert(submission)
     for subreddit in subreddits:
-        submissions = get_submissions(subreddit)
+        submissions = add_submissions(subreddit)
         for submission in submissions:
             table.insert(submission)
+    
